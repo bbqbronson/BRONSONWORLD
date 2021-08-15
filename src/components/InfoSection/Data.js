e0fdd6ef8e00ba618db5591ce8a81d6d3a46cef0
Bronson Perez <bperez1081@gmail.com>
2021-08-14 20:57:15 -0700

    typo

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,7 +6,7 @@ export const homeObjOne = {
   topLine: 'About Me',
   headline: 'Bronson Perez',
   description:
-    'I am a full stack developer who loves combining the frictionless design features of a UI/UX front end and the data in the back end. I recently earned a certificate in full stack development from UCI. I have worked on teams to provide the best user experience possible, and while incorporating modern design elements. My passion is to use my skills to build software/websites for the everyday person.',
+    'I am a full stack developer who loves combining the frictionless design features of a UI/UX front end and the data in the back end. I recently earned a certificate in full stack development from UCI. I have worked on teams to provide the best user experience possible, while incorporating modern design elements. My passion is to use my skills to build software/websites for the everyday person.',
   buttonLabel: 'Checkout My Linkedin',
   imgStart: false,
   img: require('../../images/guts.png'),
